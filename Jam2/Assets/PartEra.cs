24fb36680a24fe23301f2791c1677bc0fa9c1b4f
MadnessX <tansen.zhu@gmail.com>
2013-05-08 00:49:51 -0700

    Finished PartGenerator, misc. changes for code-ability

--- Jam2/Assets/PartEra.cs
+++ Jam2/Assets/PartEra.cs
@@ -6,34 +6,34 @@ public class PartEra {
     public int yearStart;
     public int yearEnd;
 
-    public Pair<int, int> cpu_CoresRange;
-    public Pair<int, int> cpu_ClockRange;
-    public Pair<int, int> cpu_BitsRange;
+    public Pair<int,int> cpu_CoresRange { get; set; }
+    public Pair<int,int> cpu_ClockRange { get; set; }
+    public Pair<int,int> cpu_BitsRange { get; set; }
 
-    public Pair<int, int> gpu_MegaflopsRange;
-    public Pair<int, int> gpu_MemoryRange;
+    public Pair<int,int> gpu_MegaflopsRange { get; set; }
+    public Pair<int,int> gpu_MemoryRange { get; set; }
 
-    public Pair<int, int> hdd_SizeRange;
-    public Pair<int, int> hdd_SpeedRange;
+    public Pair<int,int> hdd_SizeRange { get; set; }
+    public Pair<int,int> hdd_SpeedRange { get; set; }
 
-    public Pair<int, int> ram_SizeRange;
-    public Pair<int, int> ram_SpeedRange;
-    public Pair<int, int> ram_ChannelsRange;
+    public Pair<int,int> ram_SizeRange { get; set; }
+    public Pair<int,int> ram_SpeedRange { get; set; }
+    public Pair<int,int> ram_ChannelsRange { get; set; }
 
-    public Pair<int, int> input_DPIRange;
-    public Pair<int, int> input_PollingRange;
+    public Pair<int,int> input_DPIRange { get; set; }
+    public Pair<int,int> input_PollingRange { get; set; }
 
-    public Pair<int, int> output_ResolutionRange; //May want to make this a vector of discrete values
-    public Pair<int, int> output_RefreshRange; //May want to make this a vector of discrete values
-    public Pair<int, int> output_SQRange;
+    public Pair<int,int> output_ResolutionRange { get; set; }
+    public Pair<int,int> output_RefreshRange { get; set; }
+    public Pair<int,int> output_SQRange { get; set; }
 
-    public Pair<NetworkType, NetworkType> network_TypeRange;
-    public Pair<int, int> network_PingRange;
-    public Pair<int, int> network_BandwidthRange;
+    public Pair<NetworkType,NetworkType> network_TypeRange { get; set; }
+    public Pair<int,int> network_PingRange { get; set; }
+    public Pair<int, int> network_BandwidthRange { get; set; }
 
-    public Pair<int, int> pSupply_wattageRange;
+    public Pair<int,int> pSupply_WattageRange { get; set; }
 
-    public Pair<int, int> chassis_CoolRange;
+    public Pair<int, int> chassis_CoolRange { get; set; }
 
     public PartEra(Era era, int yearStart, int yearEnd)
     {
@@ -45,29 +45,31 @@ public class PartEra {
 
 public class Eras
 {
-    private ArrayList eraList = new ArrayList();
+    public ArrayList eraList = new ArrayList();
+    public PartEra PE1980 = new PartEra(Era.E1980, 1980, 1990);
+
     public Eras()
     {
         //1980
-        PartEra PE1980 = new PartEra(Era.E1980, 1980, 1990);
+        PE1980 = new PartEra(Era.E1980, 1980, 1990);
         PE1980.cpu_CoresRange           = new Pair<int, int>(1,2);
         PE1980.cpu_ClockRange           = new Pair<int, int>(6,50);
         PE1980.cpu_BitsRange            = new Pair<int, int>(32,32);
 
-        PE1980.gpu_MegaflopsRange       = new Pair<int, int>(1,4); //Floats?
-        PE1980.gpu_MemoryRange          = new Pair<int, int>(1,2); //Floats
+        PE1980.gpu_MegaflopsRange       = new Pair<int, int>(1,4);
+        PE1980.gpu_MemoryRange          = new Pair<int, int>(1,2);
 
-        PE1980.hdd_SizeRange            = new Pair<int, int>(1,2); //Floats?
+        PE1980.hdd_SizeRange            = new Pair<int, int>(1,2);
         PE1980.hdd_SpeedRange           = new Pair<int, int>(600,3200);
 
-        PE1980.ram_SizeRange            = new Pair<int, int>(1,2); //Floats?
+        PE1980.ram_SizeRange            = new Pair<int, int>(1,2);
         PE1980.ram_SpeedRange           = new Pair<int, int>(1,3);
         PE1980.ram_ChannelsRange        = new Pair<int, int>(1,4);
 
         PE1980.input_DPIRange           = new Pair<int, int>(200,300);
         PE1980.input_PollingRange       = new Pair<int, int>(10,60);
 
-        PE1980.output_ResolutionRange   = new Pair<int, int>(320,640); //May want to make this a vector of discrete values
+        PE1980.output_ResolutionRange   = new Pair<int, int>(205, 480); //May want to make this a vector of discrete values
         PE1980.output_RefreshRange      = new Pair<int, int>(20, 60); ; //May want to make this a vector of discrete values
         PE1980.output_SQRange           = new Pair<int, int>(1,3);
 
@@ -75,7 +77,7 @@ public class Eras
         PE1980.network_PingRange        = new Pair<int, int>(100,300);
         PE1980.network_BandwidthRange   = new Pair<int, int>(1, 2);
 
-        PE1980.pSupply_wattageRange     = new Pair<int, int>(100, 200);
+        PE1980.pSupply_WattageRange     = new Pair<int, int>(100, 200);
 
         PE1980.chassis_CoolRange        = new Pair<int, int>(1,2);
 
--- Jam2/Assets/PartGenerator.cs
+++ Jam2/Assets/PartGenerator.cs
@@ -61,86 +61,141 @@ public class PartGenerator : MonoBehaviour {
     Hashtable OUTPUT = new Hashtable();
     Hashtable NETWORK = new Hashtable();
 
-    Hashtable RANGE = new Hashtable();
-    Hashtable E1980 = new Hashtable();
-    Hashtable E1990 = new Hashtable();
-    Hashtable E2000 = new Hashtable();
-    Hashtable E2010 = new Hashtable();
-    Hashtable E2020 = new Hashtable();
-    Hashtable E2030 = new Hashtable();
-
-    Hashtable PRICE = new Hashtable();
+    ArrayList MOBO_PAIRS = new ArrayList();
+    ArrayList CPU_PAIRS = new ArrayList();
+    ArrayList GPU_PAIRS = new ArrayList();
+    ArrayList RAM_PAIRS = new ArrayList();
+    ArrayList HDD_PAIRS = new ArrayList();
+    ArrayList PSU_PAIRS = new ArrayList();
+    ArrayList CHASSIS_PAIRS = new ArrayList();
+    ArrayList INPUT_PAIRS = new ArrayList();
+    ArrayList OUTPUT_PAIRS = new ArrayList();
+    ArrayList NETWORK_PAIRS = new ArrayList();
 
     Era currentEra;
     System.Random rand;
+    Eras ranges;
 
     void Start()
     {
         MOBO[Company.EDVI] = 1.0f;
+        MOBO_PAIRS.Add(new Pair<PartType,Company>(PartType.MOBO,Company.EDVI));
         MOBO[Company.MPI] = 0.95f;
+        MOBO_PAIRS.Add(new Pair<PartType, Company>(PartType.MOBO, Company.MPI));
         MOBO[Company.AND] = 0.9f;
+        MOBO_PAIRS.Add(new Pair<PartType, Company>(PartType.MOBO, Company.AND));
         MOBO[Company.PINTEL] = 0.85f;
+        MOBO_PAIRS.Add(new Pair<PartType, Company>(PartType.MOBO, Company.PINTEL));
         MOBO[Company.GIGAFLOP] = 0.8f;
+        MOBO_PAIRS.Add(new Pair<PartType, Company>(PartType.MOBO, Company.GIGAFLOP));
 
         CPU[Company.PINTEL] = 1.0f;
+        CPU_PAIRS.Add(new Pair<PartType, Company>(PartType.CPU, Company.PINTEL));
         CPU[Company.AND] = 0.95f;
+        CPU_PAIRS.Add(new Pair<PartType, Company>(PartType.CPU, Company.AND));
         CPU[Company.MACROSOFT] = 0.9f;
+        CPU_PAIRS.Add(new Pair<PartType, Company>(PartType.CPU, Company.MACROSOFT));
         CPU[Company.EASTERN] = 0.85f;
+        CPU_PAIRS.Add(new Pair<PartType, Company>(PartType.CPU, Company.EASTERN));
         CPU[Company.COMPUTECH] = 0.8f;
+        CPU_PAIRS.Add(new Pair<PartType, Company>(PartType.CPU, Company.COMPUTECH));
 
         GPU[Company.EDVI] = 1.0f;
+        GPU_PAIRS.Add(new Pair<PartType, Company>(PartType.GPU, Company.EDVI));
         GPU[Company.MPI] = 0.95f;
+        GPU_PAIRS.Add(new Pair<PartType, Company>(PartType.GPU, Company.MPI));
         GPU[Company.GIGAFLOP] = 0.95f;
+        GPU_PAIRS.Add(new Pair<PartType, Company>(PartType.GPU, Company.GIGAFLOP));
         GPU[Company.LASUS] = 0.9f;
+        GPU_PAIRS.Add(new Pair<PartType, Company>(PartType.GPU, Company.LASUS));
         GPU[Company.AND] = 0.85f;
+        GPU_PAIRS.Add(new Pair<PartType, Company>(PartType.GPU, Company.AND));
         GPU[Company.LOGIBLAT] = 0.8f;
+        GPU_PAIRS.Add(new Pair<PartType, Company>(PartType.GPU, Company.LOGIBLAT));
 
         RAM[Company.QUEENSTON] = 1.0f;
+        RAM_PAIRS.Add(new Pair<PartType, Company>(PartType.RAM, Company.QUEENSTON));
         RAM[Company.MAURADER] = 0.95f;
+        RAM_PAIRS.Add(new Pair<PartType, Company>(PartType.RAM, Company.MAURADER));
         RAM[Company.ESSENTIAL] = 0.9f;
+        RAM_PAIRS.Add(new Pair<PartType, Company>(PartType.RAM, Company.ESSENTIAL));
         RAM[Company.EASTERN] = 0.85f;
+        RAM_PAIRS.Add(new Pair<PartType, Company>(PartType.RAM, Company.EASTERN));
         RAM[Company.ORANGE] = 0.8f;
+        RAM_PAIRS.Add(new Pair<PartType, Company>(PartType.RAM, Company.ORANGE));
 
         HDD[Company.QUEENSTON] = 1.0f;
+        HDD_PAIRS.Add(new Pair<PartType, Company>(PartType.HDD, Company.QUEENSTON));
         HDD[Company.EASTERN] = 1.0f;
+        HDD_PAIRS.Add(new Pair<PartType, Company>(PartType.HDD, Company.EASTERN));
         HDD[Company.PINTEL] = 0.95f;
+        HDD_PAIRS.Add(new Pair<PartType, Company>(PartType.HDD, Company.PINTEL));
         HDD[Company.ESSENTIAL] = 0.9f;
+        HDD_PAIRS.Add(new Pair<PartType, Company>(PartType.HDD, Company.ESSENTIAL));
         HDD[Company.ORANGE] = 0.85f;
+        HDD_PAIRS.Add(new Pair<PartType, Company>(PartType.HDD, Company.ORANGE));
         HDD[Company.LASUS] = 0.8f;
+        HDD_PAIRS.Add(new Pair<PartType, Company>(PartType.HDD, Company.LASUS));
 
         PSU[Company.ANT_TECH] = 1.0f;
+        PSU_PAIRS.Add(new Pair<PartType, Company>(PartType.PSU, Company.ANT_TECH));
         PSU[Company.MPI] = 0.95f;
+        PSU_PAIRS.Add(new Pair<PartType, Company>(PartType.PSU, Company.MPI));
         PSU[Company.MAURADER] = 0.9f;
+        PSU_PAIRS.Add(new Pair<PartType, Company>(PartType.PSU, Company.MAURADER));
         PSU[Company.ESSENTIAL] = 0.85f;
+        PSU_PAIRS.Add(new Pair<PartType, Company>(PartType.PSU, Company.ESSENTIAL));
         PSU[Company.GIGAFLOP] = 0.8f;
+        PSU_PAIRS.Add(new Pair<PartType, Company>(PartType.PSU, Company.GIGAFLOP));
 
         CHASSIS[Company.ANT_TECH] = 1.0f;
+        CHASSIS_PAIRS.Add(new Pair<PartType, Company>(PartType.CHASSIS, Company.ANT_TECH));
         CHASSIS[Company.MAURADER] = 0.95f;
+        CHASSIS_PAIRS.Add(new Pair<PartType, Company>(PartType.CHASSIS, Company.MAURADER));
         CHASSIS[Company.MPI] = 0.9f;
+        CHASSIS_PAIRS.Add(new Pair<PartType, Company>(PartType.CHASSIS, Company.MPI));
         CHASSIS[Company.GIGAFLOP] = 0.85f;
+        CHASSIS_PAIRS.Add(new Pair<PartType, Company>(PartType.CHASSIS, Company.GIGAFLOP));
         CHASSIS[Company.COMPUTECH] = 0.8f;
+        CHASSIS_PAIRS.Add(new Pair<PartType, Company>(PartType.CHASSIS, Company.COMPUTECH));
 
         INPUT[Company.PHAZER] = 1.0f;
+        INPUT_PAIRS.Add(new Pair<PartType, Company>(PartType.INPUT, Company.PHAZER));
         INPUT[Company.LOGIBLAT] = 0.95f;
+        INPUT_PAIRS.Add(new Pair<PartType, Company>(PartType.INPUT, Company.LOGIBLAT));
         INPUT[Company.MACROSOFT] = 0.9f;
+        INPUT_PAIRS.Add(new Pair<PartType, Company>(PartType.INPUT, Company.MACROSOFT));
         INPUT[Company.ORANGE] = 0.85f;
+        INPUT_PAIRS.Add(new Pair<PartType, Company>(PartType.INPUT, Company.ORANGE));
         INPUT[Company.MAURADER] = 0.8f;
+        INPUT_PAIRS.Add(new Pair<PartType, Company>(PartType.INPUT, Company.MAURADER));
 
         OUTPUT[Company.PHAZER] = 1.0f;
+        OUTPUT_PAIRS.Add(new Pair<PartType, Company>(PartType.OUTPUT, Company.PHAZER));
         OUTPUT[Company.ORANGE] = 0.95f;
+        OUTPUT_PAIRS.Add(new Pair<PartType, Company>(PartType.OUTPUT, Company.ORANGE));
         OUTPUT[Company.LASUS] = 0.95f;
+        OUTPUT_PAIRS.Add(new Pair<PartType, Company>(PartType.OUTPUT, Company.LASUS));
         OUTPUT[Company.EDVI] = 0.9f;
+        OUTPUT_PAIRS.Add(new Pair<PartType, Company>(PartType.OUTPUT, Company.EDVI));
         OUTPUT[Company.LOGIBLAT] = 0.85f;
+        OUTPUT_PAIRS.Add(new Pair<PartType, Company>(PartType.OUTPUT, Company.LOGIBLAT));
         OUTPUT[Company.COMPUTECH] = 0.8f;
+        OUTPUT_PAIRS.Add(new Pair<PartType, Company>(PartType.OUTPUT, Company.COMPUTECH));
 
         NETWORK[Company.CHRONO] = 1.0f;
+        NETWORK_PAIRS.Add(new Pair<PartType, Company>(PartType.NETWORK, Company.CHRONO));
         NETWORK[Company.SINGULAR] = 0.95f;
+        NETWORK_PAIRS.Add(new Pair<PartType, Company>(PartType.NETWORK, Company.SINGULAR));
         NETWORK[Company.BOMBAST] = 0.9f;
+        NETWORK_PAIRS.Add(new Pair<PartType, Company>(PartType.NETWORK, Company.BOMBAST));
+
+        ranges = new Eras();
     }
 
-    public Part generatePart(Type partType, Company company)
+    public object generatePart(Type partType, Company company)
     {
-        Part returnPart = null;
+        object returnPart = null;
         if (partType == typeof(CPU))
         {
             /*
@@ -153,30 +208,141 @@ public class PartGenerator : MonoBehaviour {
                 1
                 )
              * */
+
+            returnPart = new CPU(generatePartName(partType, company),
+                company,
+                500.0f * (float)CPU[company] * (float)CPU[company],
+                "Debug",
+                100 * (int)CPU[company],
+                1.0f,
+                rand.Next(ranges.PE1980.cpu_CoresRange.getLeft(), ranges.PE1980.cpu_CoresRange.getRight() + 1),
+                rand.Next(ranges.PE1980.cpu_ClockRange.getLeft(), ranges.PE1980.cpu_ClockRange.getRight() + 1),
+                rand.Next(ranges.PE1980.cpu_BitsRange.getLeft(), ranges.PE1980.cpu_BitsRange.getRight() + 1)
+                );
+
         }
         else if (partType == typeof(GPU))
         {
+            returnPart = new GPU(generatePartName(partType, company),
+                company,
+                 500.0f * (float)GPU[company] * (float)GPU[company],
+                "Debug",
+                200 * (int)GPU[company],
+                1.0f,
+                rand.Next(ranges.PE1980.gpu_MegaflopsRange.getLeft(), ranges.PE1980.gpu_MegaflopsRange.getRight() + 1),
+                rand.Next(ranges.PE1980.gpu_MemoryRange.getLeft(), ranges.PE1980.gpu_MemoryRange.getRight() + 1)
+                );
         }
         else if (partType == typeof(HDD))
         {
+            returnPart = new HDD(generatePartName(partType, company),
+                company,
+                 500.0f * (float)HDD[company] * (float)HDD[company],
+                "Debug",
+                5 * (int)HDD[company],
+                1.0f,
+                rand.Next(ranges.PE1980.hdd_SizeRange.getLeft(), ranges.PE1980.hdd_SizeRange.getRight() + 1),
+                rand.Next(ranges.PE1980.hdd_SpeedRange.getLeft(), ranges.PE1980.hdd_SpeedRange.getRight() + 1)
+                );
         }
         else if (partType == typeof(RAM))
         {
+            returnPart = new RAM(generatePartName(partType, company),
+                company,
+                 500.0f * (float)RAM[company] * (float)RAM[company],
+                "Debug",
+                5 * (int)RAM[company],
+                1.0f,
+                rand.Next(ranges.PE1980.ram_SizeRange.getLeft(), ranges.PE1980.ram_SizeRange.getRight() + 1),
+                rand.Next(ranges.PE1980.ram_SpeedRange.getLeft(), ranges.PE1980.ram_SpeedRange.getRight() + 1),
+                rand.Next(ranges.PE1980.ram_ChannelsRange.getLeft(), ranges.PE1980.ram_ChannelsRange.getRight() + 1)
+                );
         }
         else if (partType == typeof(CompInput))
         {
+            returnPart = new CompInput(generatePartName(partType, company),
+                company,
+                 500.0f * (float)INPUT[company] * (float)INPUT[company],
+                "Debug",
+                5 * (int)INPUT[company],
+                1.0f,
+                rand.Next(ranges.PE1980.input_DPIRange.getLeft(), ranges.PE1980.input_DPIRange.getRight() + 1),
+                rand.Next(ranges.PE1980.input_PollingRange.getLeft(), ranges.PE1980.input_PollingRange.getRight() + 1),
+                rand.Next(10) <= 7 ? false : true
+                );
         }
         else if (partType == typeof(CompOutput))
         {
+            returnPart = new CompOutput(generatePartName(partType, company),
+                company,
+                 500.0f * (float)OUTPUT[company] * (float)OUTPUT[company],
+                "Debug",
+                50 * (int)OUTPUT[company],
+                1.0f,
+                rand.Next(ranges.PE1980.output_ResolutionRange.getLeft(), ranges.PE1980.output_ResolutionRange.getRight() + 1),
+                rand.Next(ranges.PE1980.output_RefreshRange.getLeft(), ranges.PE1980.output_RefreshRange.getRight() + 1),
+                rand.Next(ranges.PE1980.output_SQRange.getLeft(), ranges.PE1980.output_SQRange.getRight() + 1)
+                );
         }
         else if (partType == typeof(CompNetwork))
         {
+            returnPart = new CompNetwork(generatePartName(partType, company),
+                company,
+                 500.0f * (float)NETWORK[company] * (float)NETWORK[company],
+                "Debug",
+                0 * (int)NETWORK[company],
+                1.0f,
+                rand.Next(2) == 0 ? ranges.PE1980.network_TypeRange.getLeft() : ranges.PE1980.network_TypeRange.getRight(),
+                rand.Next(ranges.PE1980.network_PingRange.getLeft(), ranges.PE1980.network_PingRange.getRight() + 1),
+                rand.Next(ranges.PE1980.network_BandwidthRange.getLeft(), ranges.PE1980.network_BandwidthRange.getRight() + 1)
+                );
         }
         else if (partType == typeof(PowerSupply))
         {
+            returnPart = new PowerSupply(generatePartName(partType, company),
+                company,
+                 500.0f * (float)PSU[company] * (float)PSU[company],
+                "Debug",
+                0 * (int)PSU[company],
+                1.0f,
+                rand.Next(ranges.PE1980.pSupply_WattageRange.getLeft(), ranges.PE1980.pSupply_WattageRange.getRight() + 1)
+                );
         }
         else if (partType == typeof(Chassis))
         {
+            returnPart = new Chassis(generatePartName(partType, company),
+                company,
+                 500.0f * (float)CHASSIS[company] * (float)CHASSIS[company],
+                "Debug",
+                0 * (int)CHASSIS[company],
+                1.0f,
+                rand.Next(ranges.PE1980.chassis_CoolRange.getLeft(), ranges.PE1980.chassis_CoolRange.getRight() + 1)
+                );
+        }
+        else if (partType == typeof(Motherboard))
+        {
+            returnPart = new Motherboard(generatePartName(partType, company),
+                500.0f * (float)MOBO[company] * (float)MOBO[company],
+                1.0f,
+                "Debug",
+                "Debug",
+                "Debug",
+                "Debug",
+                "Debug",
+                "Debug",
+                "Debug",
+                "Debug",
+                "Debug",
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null
+                );
         }
         return returnPart;
     }
@@ -236,7 +402,7 @@ public class PartGenerator : MonoBehaviour {
         }
         if (company == Company.ANT_TECH)
         {
-            companyName = "Ant Tech";
+            companyName = "Ant-Tech";
         }
         if (company == Company.BOMBAST)
         {
